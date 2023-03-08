a2497ebe06bf411b5217bc3e21426d09cd8f0145
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2023-03-08 14:40:33 +0100

    Added: described docker build kafka images script

--- part1/docker-build-kafka-images.ps1
+++ part1/docker-build-kafka-images.ps1
@@ -1,8 +1,6 @@
-# build each image in the following folders
-# - .\cpu_logger_kafka
-# - .\mongodb_consumer_kafka
-# - .\sensor_logger_fast_api_kafka
+# this script builds my docker images for the kafka service-stack
 
+# add the images you want to build to the array
 $images = @(
     "cpu_logger_kafka",
     "mongodb_consumer_kafka",
