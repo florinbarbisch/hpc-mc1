4f5b8753bd4e0ce54a50c934dda64206166d4237
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2023-03-19 19:30:47 +0100

    Added: multiplier to overload Kafka

--- part1/sensor_logger_fast_api_kafka/app/main.py
+++ part1/sensor_logger_fast_api_kafka/app/main.py
@@ -6,6 +6,9 @@ import logging
 
 logger = logging.getLogger(__name__)  # the __name__ resolve to "uicheckapp.services"
 
+# this is a hack to multiply the work-load inserted into Kafka
+names_to_multiply = os.environ.get('NAMES_TO_MULTIPLY', '').split(",")
+
 _producer = None
 try:
     _producer = KafkaProducer(bootstrap_servers=os.environ.get('KAFKA_BROKER', 'broker1:9093').split(","), 
@@ -31,12 +34,13 @@ async def data(data: dict, activity: str = None, device: str = None, person: str
     try:
         for measurement in data["payload"]:
             timestamp_ms = int(measurement["time"]) // 1_000_000 # convert nanoseconds to milliseconds
-            key_bytes = bytes(f"{person}:{device}:{activity}", encoding='utf-8')
             value_bytes = bytes(json.dumps(measurement["values"]), encoding='utf-8')
-            _producer.send(measurement['name'],
-                           key=key_bytes,
-                           value=value_bytes,
-                           timestamp_ms=timestamp_ms) 
+            for name in names_to_multiply + [person]:
+                key_bytes = bytes(f"{name}:{device}:{activity}", encoding='utf-8')
+                _producer.send(measurement['name'],
+                            key=key_bytes,
+                            value=value_bytes,
+                            timestamp_ms=timestamp_ms) 
         _producer.flush()
         logger.info('Message published successfully.')
     except Exception as ex:
